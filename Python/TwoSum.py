25cc2ca86fde30cc6f501263f6b9e1864c3c7a58
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-15 11:28:38 +0530

    Update and rename Two Sum.py to TwoSum.py

---
+++ Python/TwoSum.py
@@ -0,0 +1,21 @@
+'''
+Question : Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+           You may assume that each input would have exactly one solution, and you may not use the same element twice.
+           You can return the answer in any order.
+
+Link : https://leetcode.com/problems/two-sum/
+'''
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        Mapped = {} 
+        for i , n in enumerate(nums):
+            dif = target - n
+            if dif in Mapped:
+                return [Mapped[dif] , i]
+            Mapped[n] = i
