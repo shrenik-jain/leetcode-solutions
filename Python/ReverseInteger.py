da96664af19c8563a1a6c50f6027690a703b1e6c
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-15 11:34:35 +0530

    Update and rename Reverse Integer.py to ReverseInteger.py

--- Python/ReverseInteger.py
+++ Python/ReverseInteger.py
@@ -1,6 +1,13 @@
+'''
+Question : Given a 32-bit signed integer, reverse digits of an integer.
+
+Link : https://leetcode.com/problems/reverse-integer/
+'''
+
 class Solution:
     def reverse(self, x: int) -> int:
         rev = int(str(abs(x))[::-1])
+        
         if rev.bit_length()<32:
             if x <0:
                 return -rev
