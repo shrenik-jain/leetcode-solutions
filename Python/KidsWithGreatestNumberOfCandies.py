db885d91a051bec0df27fb1c13e2e665b8c88e9c
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-10-22 17:20:38 +0530

    Minor updates to the script

---
+++ Python/KidsWithGreatestNumberOfCandies.py
@@ -0,0 +1,12 @@
+'''
+Question : Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+           For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number 
+           of candies among them. Notice that multiple kids can have the greatest number of candies.
+
+Link : https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+'''
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+            most = max(candies)
+            return [(candy + extraCandies) >= most for candy in candies]
