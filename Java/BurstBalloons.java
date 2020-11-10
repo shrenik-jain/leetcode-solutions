c7a724d4cd0618ced48744128dfe6f2d7b817349
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-11-10 16:27:20 +0530

    Solution for a Hard LeetCode Problem

--- Java/BurstBalloons.java
+++ Java/BurstBalloons.java
@@ -1,7 +1,10 @@
 /*
-Question : 
+Question : Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented by array nums. 
+           You are asked to burst all the balloons. If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins.
+           Here left and right are adjacent indices of i. After the burst, the left and right then becomes adjacent.
+           Find the maximum coins you can collect by bursting the balloons wisely.
 
-Link :
+Link : https://leetcode.com/problems/burst-balloons/
 */
 
 class Solution {
