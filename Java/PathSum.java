56b60544e62cf44ed5ab5b5bd281256f28854588
Shrenik Jain <shrenikkjain81@gmail.com>
2021-06-11 13:02:25 +0530

    Added new java leetcode solution

---
+++ Java/PathSum.java
@@ -0,0 +1,38 @@
+/*
+Question : Given the root of a binary tree and an integer targetSum, return true if the tree has a 
+           root-to-leaf path such that adding up all the values along the path equals targetSum.
+           A leaf is a node with no children.
+
+Link : https://leetcode.com/problems/path-sum/
+*/
+
+/*
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null)
+            return false;
+        
+        targetSum = targetSum - root.val;
+        
+        if (targetSum == 0 && root.left == null && root.right == null) 
+            return true;
+        
+        return hasPathSum(root.left, targetSum) || 
+               hasPathSum(root.right, targetSum);
+    }
+}
